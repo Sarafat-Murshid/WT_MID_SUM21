c19fb5e49119e97ef3657c258581a45dfecdf197
Sarafat Murshid <sarafatmurshid@gmail.com>
2021-06-22 14:01:08 +0600

    Project Forms

---
+++ Project/doctor.php
@@ -0,0 +1,45 @@
+<html>
+	<head>
+	<title>Doctor</title>
+	</head>
+	
+	<body>
+		<div style="border:1px solid black">
+			<h5 align="left"><a href ="homepage.php">homepage</a></h5>
+			<h1 align="center">Doctor</h1>
+		</div>
+		<br>
+		<div style="border:1px solid black">
+		<h2 align="left">Doctor details</h2>
+		</div>
+		<br>
+		<div>
+		<table width="100%" border="2">
+			<tr>
+			<td>Id</td>
+			<td>Name</td>
+			<td>Department</td>
+			<td>Consult Time</td>
+			<td>Fees</td>
+			<td>Profile</td>
+			</tr>
+			<tr>
+			<td>01</td>
+			<td>Sarafat Murshid</td>
+			<td>Gastroenterologist</td>
+			<td>5.00 pm - 9.00 pm</td>
+			<td>500tk</td>
+			<td><a href="S.php">Sarafat Murshid</a></td>
+			</tr>
+			<tr>
+			<td>02</td>
+			<td>Haidar Ali</td>
+			<td>cardiologist</td>
+			<td>4.00 pm - 9.00 pm</td>
+			<td>800tk</td>
+			<td><a href="H.php">Haidar Ali</a></td>
+			</tr>
+		</table>
+		</div>
+	</body>
+</html>
